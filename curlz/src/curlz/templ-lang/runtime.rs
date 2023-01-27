5425eb918f6063e2590d7555aaa4a84019dcecd4
Sven Kanoldt <sven@d34dl0ck.me>
2023-01-27 09:56:01 +0100

    feat(experimental:template-language): start an own template language
    The whole reason for this experiment is to get closer to the http plugin template syntax as linked in #5
    There the vscode plugin [has this concept of system variables](https://marketplace.visualstudio.com/items?itemName=humao.rest-client#system-variables) that follow the syntax of a mix of expression and function e.g.
    
    - {{$randomInt min max}}: Returns a random integer between min (included) and max (excluded)
    - {{$dotenv [%]variableName}}: Returns the environment value stored in the .env file which exists in the same directory of your .http file.
    
    Both examples show that `$<ident>` is similar to a function name and then a variable argument list is passed without any braces like `()`.
    
    This experiment focuses on the ability to register functions for this very system variable syntax at compile time in an extensible fashion.
    
    done so far:
    - lexing and parsing of very basic templates with an expression `{{ var }}`
    - basic runtime to interpret the AST
    - some tests added
    - design of a Visitor pattern for the AST
    
    yet open
    - [ ] tests for unhappy path are to less
    - [ ] runtime is very incomplete yet, `SysVar` hooked functions are missing e.g. `hello {{ $processEnv HOME }}`

---
+++ curlz/src/curlz/templ-lang/runtime.rs
@@ -0,0 +1,113 @@
+use crate::language::ast;
+use crate::language::ast_visitor::{AstVisit, AstVisitAcceptor};
+use std::borrow::Cow;
+use std::collections::HashMap;
+
+#[derive(Default)]
+struct Runtime<'source> {
+    // todo: maybe an `Ident` should be the key for variables
+    vars: HashMap<&'source str, ast::Value>,
+    output: Vec<u8>,
+}
+
+impl<'source> Runtime<'source> {
+    /// registers a variable with a given `id` that is the variable identifier
+    pub fn with_variable(mut self, id: &'source str, var: impl Into<ast::Value>) -> Self {
+        self.vars.insert(id, var.into());
+
+        self
+    }
+
+    /// returns the rendered template as a string in form of a `Cow<'_, str>`
+    pub fn rendered(&mut self) -> Cow<'_, str> {
+        String::from_utf8_lossy(self.output.as_slice())
+    }
+
+    #[cfg(test)]
+    pub fn render(&mut self, source: &'source str) -> Cow<'_, str> {
+        use crate::language::parser::Parser;
+
+        let parsed = Parser::new(source).parse().unwrap();
+        parsed.accept(self);
+
+        self.rendered()
+    }
+}
+
+impl<'source> AstVisit<'source> for Runtime<'source> {
+    fn visit_stmt(&mut self, stmt: &ast::Stmt<'source>) {
+        use ast::Stmt::*;
+
+        match stmt {
+            Template(spanned) => {
+                for s in spanned.node.children.as_slice() {
+                    s.accept(self);
+                }
+            }
+            EmitRaw(spanned) => spanned.node.accept(self),
+            EmitExpr(spanned) => spanned.node.expr.accept(self),
+        }
+    }
+
+    fn visit_expr(&mut self, expr: &ast::Expr<'source>) {
+        use ast::Expr::*;
+
+        match expr {
+            SysVar(_var) => todo!(),
+            Var(var) => {
+                if let Some(var) = self.vars.get(var.node.id) {
+                    self.output
+                        .extend_from_slice(var.as_str().unwrap().as_bytes());
+                }
+            }
+            Const(_) => {
+                todo!()
+            }
+            Call(_) => {
+                todo!()
+            }
+        }
+    }
+
+    fn visit_emit_raw(&mut self, raw: &ast::EmitRaw<'source>) {
+        self.output.extend_from_slice(raw.raw.as_bytes());
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::language::ast::IntoSpanned;
+
+    #[test]
+    fn test_expr_var() {
+        let mut runtime = Runtime::default().with_variable("foo", "John");
+        let expr = ast::Expr::Var(ast::Var { id: "foo" }.spanned());
+
+        expr.accept(&mut runtime);
+        assert_eq!(runtime.rendered(), "John");
+    }
+
+    #[test]
+    fn test_expr_sys_var() {
+        assert_eq!(
+            Runtime::default().render("{{ $processEnv HOME }}"),
+            env!("HOME")
+        );
+    }
+
+    #[test]
+    fn test_whole_template() {
+        assert_eq!(
+            Runtime::default()
+                .with_variable("world", "John")
+                .render("hello {{ world }}"),
+            "hello John"
+        );
+    }
+
+    #[test]
+    fn test_whole_template_unhappy() {
+        assert_eq!(Runtime::default().render("hello {{ world }}"), "hello ");
+    }
+}
